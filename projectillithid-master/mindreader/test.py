279b8bd8fb66f05e33f1cd25e7c7902888a31ed4
Ag3ntN <yunosuke.nakamura@gmail.com>
2025-03-24 14:24:49 +0900

    added graphing to test.py

--- projectillithid-master/mindreader/test.py
+++ projectillithid-master/mindreader/test.py
@@ -1,9 +1,49 @@
 import mindwave, time
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Initialize headset
 headset = mindwave.Headset('COM7')
 time.sleep(2)
 
-while True:
-    time.sleep(.1)
-    print ("Raw value: %s, Attention: %s, Meditation: %s" % (headset.raw_value, headset.attention, headset.meditation))
-    print ("Waves: {}".format(headset.waves))
+# Initialize plot
+plt.ion()  # Turn on interactive mode
+fig, ax = plt.subplots()
+x_data, y_data = [], []
+line, = ax.plot(x_data, y_data, 'b-')  # Blue line
+
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Attention Level')
+ax.set_ylim(0, 100)  # Attention values range from 0-100
+ax.set_title('Mindwave Attention Level Over Time')
+
+start_time = time.time()
+
+try:
+    while True:
+        time.sleep(0.1)  # Update every 100ms
+        current_time = time.time() - start_time
+        attention_value = headset.attention
+
+        if attention_value != 0:  # Avoid plotting invalid values
+            x_data.append(current_time)
+            y_data.append(attention_value)
+
+            # Keep the last 100 points for a rolling window effect
+            if len(x_data) > 100:
+                x_data.pop(0)
+                y_data.pop(0)
+
+            # Update plot
+            line.set_xdata(x_data)
+            line.set_ydata(y_data)
+            ax.relim()  # Recalculate limits
+            ax.autoscale_view()  # Rescale view
+            plt.draw()
+            plt.pause(0.01)  # Allow time for the plot to update
+
+except KeyboardInterrupt:
+    print("Plotting stopped.")
+
+plt.ioff()  # Turn off interactive mode
+plt.show()  # Show final static plot
